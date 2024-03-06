909106132ea577e83b03af86d7b2968c11068672
crivers973 <xarsk9@gmail.com>
2024-03-06 14:35:01 -0500

    Add project files.

---
+++ Project3/Configuration.cs
@@ -0,0 +1,21 @@
+﻿/*The Configuration class reads in data from a file that determines the parameters of the
+simulation. From the configuration file, the user should be able to adjust the following:
+ The mean population size of a location at the start of the simulation.
+ The standard deviation of the population size of a location at the start of the simulation.
+ The percent chance of the disease spreading to another person at the same location.
+ The percent chance of the disease flagging a person as dead.
+ The number of hours that the disease last.
+ The number of hours that a quarantine last.
+ The mean percent chance of a person entering quarantine.
+ The standard deviation of the percent chance of a person entering quarantine.
+ How long the simulation should last.
+ The percent chance that a person will travel each hour of the simulation.
+(Note: From the configurable settings above, you may extrapolate additional requirements.
+ */
+namespace Project3
+{
+    public class Configuration
+    {
+
+    }
+}
---
+++ Project3/Location.cs
@@ -0,0 +1,19 @@
+﻿/*The Location class represents a population or community center. It has the following properties:
+ string id – A location’s unique identifier to assist with debugging and analysis.
+ ICollection<Person> people – A collection of people currently at the location.
+ ICollection<Location> neighbors – A collection of nodes connected to the current
+node.
+Each hour of the simulation, people at the same location should have an opportunity to spread the
+disease to other people at the same location. After the spreading of the disease occurs, then
+people should have the opportunity to move to a neighboring location.
+People that are dead do not spread the disease and do not travel ever again.
+People that are quarantined do not spread the disease and do not travel, but may resume both
+once the quarantine period is complete
+ */
+namespace Project3
+{
+    public class Location
+    {
+
+    }
+}
