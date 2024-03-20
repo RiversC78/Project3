d1c6de6f9d4b05305efb58fb02366780ff88655e
crivers973 <xarsk9@gmail.com>
2024-03-20 13:32:13 -0400

    create datalayer

---
+++ Project3/DataLayer.cs
@@ -0,0 +1,10 @@
+﻿namespace Project3
+{
+    /// <summary>
+    /// class to contain methods that take in and manipulte files 
+    /// </summary>
+    public class DataLayer
+    {
+        StreamReader strmrdr = new(); 
+    }
+}
